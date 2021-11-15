0f739c12bf27c55206660da530413865c84e5cf6
carcheky <carcheky@gmail.com>
2021-11-15 16:36:40 +0100

    chore(scripts): files-private perms

--- scripts/hiberus-post-install.sh
+++ scripts/hiberus-post-install.sh
@@ -4,3 +4,6 @@ echo "==> sudo chown -R wodby:wodby web"
 sudo chown -R wodby:wodby web
 echo "==> sudo chown -R www-data:www-data web/sites/default/files/"
 sudo chown -R www-data:www-data web/sites/default/files/
+echo "==> sudo chown -R www-data:www-data files-private"
+sudo chown -R www-data:www-data files-private
+drush cr
