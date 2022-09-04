56fbae358a07707e6e9e15f814c71c6fed457405
Dennis A. Torres Rodriguez <d70rr3s@gmail.com>
2022-09-04 20:19:02 +0200

    feat: update core 9.4 and configuration (also more contrib added)

--- toolbox/templates/drupal/settings.dev.php
+++ toolbox/templates/drupal/settings.dev.php
@@ -15,3 +15,13 @@ $config['field_ui.settings']['field_prefix'] = '';
  * Avoid ssl verify
  */
 $settings['http_client_config'] = ['verify' => FALSE];
+
+/**
+ * Override logging verbosity.
+ */
+$config['system.logging']['error_level'] = 'verbose';
+
+/**
+ * Increase database logging records.
+ */
+$config['dblog.settings']['row_limit'] = 10000;
